1a67600eb76771c58766729482d55e4b242a3811
FangTingKoh <41284197+FangTingKoh@users.noreply.github.com>
2019-12-19 11:06:10 +0800

    test_add..& document for TestCase
    Error can't connect to google chrome for automated screen to display.
    Requesting for help, thank you so much.
    
    Document for TestCases includes unit test case for login page and initial todo page. Both passing and failings included except justification.

---
+++ test_add_todo.py
@@ -0,0 +1,22 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+driver = webdriver.Chrome()
+driver.implicitly_wait(30)
+driver.maximize_window()
+
+def test_Navigation_application_homepage():
+    #Navigate to application home page
+    driver.get('http://localhost:8000/todo/')
+    assert "http://localhost:8000/todo/" == driver.current_url
+
+def test_todo_field():
+    #Get the input textbox
+    input_field = driver.find_element_by_name('content')
+    input_field.clear()
+    #enter input keyword
+    input_field.send_keys("Playing Final Fantasy XIII")
+    add_button = driver.find_element_by_xpath("//input[@type='submit']")
+    add_button.click()
+    assert "http://localhost:8000/todo/" == driver.current_url
+
